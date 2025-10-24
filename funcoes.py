bf289c550e2779f58040604c2ff049b4bde7bca4
lucasbsantana2007 <lucassantana220903@gmail.com>
2025-10-23 22:58:29 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -11,4 +11,5 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
     if nome_navio in frota:
         frota[nome_navio].append(posicoes)
+        if 
     return frota
