d89602673348e4a568b6d9d106174137e7064d13
lucasbsantana2007 <lucassantana220903@gmail.com>
2025-10-23 23:17:19 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,19 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
-    lista = []
+    posicoes = []
     for i in range(tamanho):
-        if orientacao == "horizontal":
-            lista.append([linha, coluna + i])
-        elif orientacao == "vertical":
-            lista.append([linha + i, coluna])
-    return lista
+        if orientacao == "vertical":
+            posicoes.append([linha + i, coluna])
+        elif orientacao == "horizontal":
+            posicoes.append([linha, coluna + i])
+    return posicoes
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    
+    return frota
